bb3c793a7c5892729f6f757103452a58135978b1
shadja <shadja4u@gmail.com>
2019-08-30 05:23:12 +0530

    adding asg and user data

--- main.tf
+++ main.tf
@@ -76,8 +76,41 @@ resource "aws_security_group" "elasticsearch-sg" {
   }
 }
 
+data "template_file" "es-bootstrap-script" {
+    template = "${file("${path.module}/files/init.tpl")}"
+}
 
+resource "aws_launch_configuration" "es-launch-conf" {
+  name_prefix          = "es_launch_config_"
+  image_id      = var.ami-id
+  instance_type = "t2.medium"
+  iam_instance_profile = "elasticsearch-ec2-discovery-role"
+  key_name = var.aws-pem-file-name
+  security_groups = ["${aws_security_group.elasticsearch-sg.id}"]
+  associate_public_ip_address = true
+  user_data = "${data.template_file.es-bootstrap-script.rendered}"
+}
 
-
+resource "aws_autoscaling_group" "es-asg" {
+  name = "${aws_launch_configuration.es-launch-conf.name}_asg"
+  launch_configuration = "${aws_launch_configuration.es-launch-conf.name}"
+  min_size             = 1
+  desired_capacity     = var.es-node-count
+  max_size             = 3
+  vpc_zone_identifier = ["${aws_subnet.es-subnet.id}"]
+  lifecycle {
+    create_before_destroy = true
+  }
+  tag {
+    key                 = "es"
+    value               = "es_node"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "name"
+    value               = "elasticsearch-node"
+    propagate_at_launch = true
+  }
+}
 
 
